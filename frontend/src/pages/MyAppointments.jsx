77a8e07cf66b088cf08985fb2e61c5c7baa772e9
Marsden <marsdenlwero@gmail.com>
2024-10-29 17:59:06 +0300

    Update

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -106,7 +106,7 @@ const MyAppointments = () => {
               <p className='text-neutral-800 font-semibold'>{item.docData.name}</p>
               <p>{item.docData.speciality}</p>
               <p className='text-zinc-700 font-medium mt-1'>Address:</p>
-              <p className='text-xs'>{item.docData.address.line1}</p>
+              <p className='text-xs'>{item.doc.address.line1}</p>
               <p className='text-xs'>{item.docData.address.line2}</p>
               <p className='text-xs mt-1'><span className='text-sm text-neutral-700 font-medium'>Date & Time:</span> {slotDateFormat(item.slotDate)} | {item.slotTime}</p>
             </div>
